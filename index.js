dbc837f08e7d8c781a7236fddc2685cfb1a206f7
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-09-20 11:06:20 +0700

    refactor: unnecessary codes

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 'use strict';
 
-const path = require('path');
 const del = require('del');
 const hasLockfile = require('has-lockfile');
 
 module.exports = cwd => {
-  cwd = path.resolve(cwd || process.cwd());
+  cwd = cwd || process.cwd();
 
   const lockfile = hasLockfile(cwd);
 
-  // Returns a promise of an array of deleted paths
   return lockfile
     ? del(['node_modules', lockfile], {cwd})
     : del('node_modules', {cwd});
 };
 
 module.exports.sync = cwd => {
-  cwd = path.resolve(cwd || process.cwd());
+  cwd = cwd || process.cwd();
 
   const lockfile = hasLockfile(cwd);
 
-  // Returns an array of deleted paths
   return lockfile
     ? del.sync(['node_modules', lockfile], {cwd})
     : del.sync('node_modules', {cwd});
