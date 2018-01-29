99081f31a5206fc88a23845dc5d578332533c8e9
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-29 15:07:55 +0700

    test: simplify

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const forceDel = require('force-del');
 const hasLockfile = require('has-lockfile');
 
 module.exports = ({ cwd = process.cwd(), lockfiles = true } = {}) => {
+  const opts = { cwd, expandDirectories: false };
+
   return lockfiles
-    ? forceDel(['node_modules'].concat(hasLockfile.lockfiles(cwd)), { cwd })
-    : forceDel(['node_modules'], { cwd });
+    ? forceDel(['node_modules'].concat(hasLockfile.lockfiles(cwd)), opts)
+    : forceDel(['node_modules'], opts);
 };
--- test.js
+++ test.js
@@ -1,112 +1,54 @@
 'use strict';
 
 const { join } = require('path');
-const { copySync, pathExistsSync, writeFileSync } = require('fs-extra');
+const { copySync, writeFileSync } = require('fs-extra');
 const fixtures = require('fixturez');
 
 const delNm = require('./');
 
+const cwd = process.cwd();
 const f = fixtures(__dirname);
 
-describe('in tempDir', async () => {
-  it('deletes `node_modules`', async () => {
-    expect.assertions(3);
-
-    const tempDir = f.temp();
-
-    writeFileSync(join(tempDir, 'package-lock.json'));
-    writeFileSync(join(tempDir, 'yarn.lock'));
-
-    copySync(
-      join(__dirname, 'node_modules', 'force-del'),
-      join(tempDir, 'node_modules')
-    );
-
-    await delNm({ cwd: tempDir, lockfiles: false });
-
-    expect(pathExistsSync(join(tempDir, 'node_modules'))).toBe(false);
-    expect(pathExistsSync(join(tempDir, 'package-lock.json'))).toBe(true);
-    expect(pathExistsSync(join(tempDir, 'yarn.lock'))).toBe(true);
-  });
-
-  it('deletes lockfiles', async () => {
-    expect.assertions(3);
-
-    const tempDir = f.temp();
-
-    writeFileSync(join(tempDir, 'package-lock.json'));
-    writeFileSync(join(tempDir, 'yarn.lock'));
-
-    await delNm({ cwd: tempDir });
-
-    expect(pathExistsSync(join(tempDir, 'node_modules'))).toBe(false);
-    expect(pathExistsSync(join(tempDir, 'package-lock.json'))).toBe(false);
-    expect(pathExistsSync(join(tempDir, 'yarn.lock'))).toBe(false);
-  });
-
-  it("doesn't delete anything", async () => {
-    expect.assertions(1);
-
-    const tempDir = f.temp();
-
-    const res = await delNm({ cwd: tempDir });
-
-    expect(res).toEqual([]);
-  });
+afterEach(() => {
+  process.chdir(cwd);
 });
 
-describe('in cwd', async () => {
-  const cwd = process.cwd();
+it('deletes `node_modules`', async () => {
+  expect.assertions(1);
 
-  afterEach(() => {
-    process.chdir(cwd);
-  });
+  const tempDir = f.temp();
+  writeFileSync(join(tempDir, 'package-lock.json'));
+  writeFileSync(join(tempDir, 'yarn.lock'));
+  copySync(
+    join(__dirname, 'node_modules', 'force-del'),
+    join(tempDir, 'node_modules')
+  );
+  process.chdir(tempDir);
 
-  it('deletes `node_modules`', async () => {
-    expect.assertions(3);
+  await expect(delNm({ lockfiles: false })).resolves.toEqual([
+    join(tempDir, 'node_modules'),
+  ]);
+});
 
-    const tempDir = f.temp();
+it('deletes lockfiles', async () => {
+  expect.assertions(1);
 
-    writeFileSync(join(tempDir, 'package-lock.json'));
-    writeFileSync(join(tempDir, 'yarn.lock'));
+  const tempDir = f.temp();
+  writeFileSync(join(tempDir, 'package-lock.json'));
+  writeFileSync(join(tempDir, 'yarn.lock'));
+  process.chdir(tempDir);
 
-    copySync(
-      join(__dirname, 'node_modules', 'force-del'),
-      join(tempDir, 'node_modules')
-    );
+  await expect(delNm()).resolves.toEqual([
+    join(tempDir, 'package-lock.json'),
+    join(tempDir, 'yarn.lock'),
+  ]);
+});
 
-    process.chdir(tempDir);
-    await delNm({ lockfiles: false });
+it("doesn't delete anything", async () => {
+  expect.assertions(1);
 
-    expect(pathExistsSync(join(tempDir, 'node_modules'))).toBe(false);
-    expect(pathExistsSync(join(tempDir, 'package-lock.json'))).toBe(true);
-    expect(pathExistsSync(join(tempDir, 'yarn.lock'))).toBe(true);
-  });
+  const tempDir = f.temp();
+  process.chdir(tempDir);
 
-  it('deletes lockfiles', async () => {
-    expect.assertions(3);
-
-    const tempDir = f.temp();
-
-    writeFileSync(join(tempDir, 'package-lock.json'));
-    writeFileSync(join(tempDir, 'yarn.lock'));
-
-    process.chdir(tempDir);
-    await delNm();
-
-    expect(pathExistsSync(join(tempDir, 'node_modules'))).toBe(false);
-    expect(pathExistsSync(join(tempDir, 'package-lock.json'))).toBe(false);
-    expect(pathExistsSync(join(tempDir, 'yarn.lock'))).toBe(false);
-  });
-
-  it("doesn't delete anything", async () => {
-    expect.assertions(1);
-
-    const tempDir = f.temp();
-
-    process.chdir(tempDir);
-    const res = await delNm();
-
-    expect(res).toEqual([]);
-  });
+  await expect(delNm()).resolves.toEqual([]);
 });
